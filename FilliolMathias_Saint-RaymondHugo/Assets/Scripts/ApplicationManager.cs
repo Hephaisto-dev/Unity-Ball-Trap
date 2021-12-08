635197fcbc0736a210c5ae75cd14262bd8bf7949
Hugo Saint-Raymond <90llumgogo@gmail.com>
2021-12-08 11:45:22 +0100

    lol

--- FilliolMathias_Saint-RaymondHugo/Assets/Scripts/ApplicationManager.cs
+++ FilliolMathias_Saint-RaymondHugo/Assets/Scripts/ApplicationManager.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ApplicationManager : MonoBehaviour
 {
-    public GameObject BirdPrefab;
-    public Transform cam;
-
-    public 
-    public float spawnRange = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +14,5 @@ public class ApplicationManager : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void SpawnFrisbee()
-    {
-            float x = cam.transform.position.x + Random.Range(-spawnRange, spawnRange);
-            float y = cam.transform.position.y + Random.Range(-spawnRange, spawnRange);
-            float z = cam.transform.position.z + Random.Range(-spawnRange, spawnRange);
-            Vector3 spawnPos = new Vector3(x, y, z);
-            InstantiateBirdPrefab, spawnPos, Quaternion.identity);
-        }
     }
 }
